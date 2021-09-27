9a045370fbf7569441e5ca6029e445b066db0be1
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2021-09-27 14:58:21 +0300

    form model

--- todo/todo/models.py
+++ todo/todo/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django import forms
 
 class Todolist(models.Model):
     todoitem = models.CharField(max_length=800)
@@ -8,3 +8,7 @@ class Todolist(models.Model):
 
     def __str__(self):
         return f"Todolist: {self.todoitem}"
+
+
+class NameForm(forms.Form):
+    your_name = forms.CharField(label='Your name', max_length=100)
--- todo/todo/views.py
+++ todo/todo/views.py
@@ -1,30 +1,37 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 from .models import Todolist
-
-
+from .models import NameForm
 
 # def index(request):
 #    return HttpResponse("Hello World!")
 
 
 def index(request):
-
-
-
-
-    list_array = []
-    #for list in Todolist.objects.all():
-    #    line = f'<p>{list.id}. {list.todoitem}</p>'
-    #    list_array.append(line)
-    #linkkiteksti = ''.join(list_array)
-
-
     context = {
         'todos': Todolist.objects.all(),
     }
     return render(request, 'todo/index.html', context)
+
+def get_name(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = NameForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/thanks/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = NameForm()
+
+    return render(request, 'todo/index.html', {'form': form})
 
 
 
